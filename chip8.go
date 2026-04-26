f035706774a0fbfd5bece900407fdff8ff913f63
jacdevv <main.jac.mil@gmail.com>
2026-04-26 16:03:13 +0800

    loadROM & factory

--- chip8.go
+++ chip8.go
@@ -1,15 +1,33 @@
 package main
 
+import "os"
+
 type Chip8 struct {
-	registers  [16]uint8
-	memory     [4096]uint8
+	registers  [16]byte
+	memory     [4096]byte
 	index      uint16
 	pc         uint16
 	stack      [16]uint16
 	sp         uint16
-	delayTimer uint8
-	soundTimer uint8
-	keypad     [16]uint8
+	delayTimer byte
+	soundTimer byte
+	keypad     [16]byte
 	video      [64 * 32]uint32
 	opcode     uint16
+}
+
+func NewChip8() *Chip8 {
+	return &Chip8{
+		pc: 0x200,
+	}
+}
+
+func (chip *Chip8) loadROM(filename string) {
+	index := 0x200
+	data, err := os.ReadFile(filename); 
+	if err != nil {
+		panic(err)
+	}
+
+	copy(chip.memory[index:], data)
 }
